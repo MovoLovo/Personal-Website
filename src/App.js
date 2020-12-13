47d6331c263a85d3c58f461f082a2d51a1175386
MovoLovo <48698410+MovoLovo@users.noreply.github.com>
2020-12-12 23:54:42 -0500

    Initialize base website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, CssBaseline } from '@material-ui/core';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline/>
+      <Container>
+        <a>Hello World</a>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
